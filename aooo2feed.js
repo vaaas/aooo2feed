e076545972bdf5efc3f15608378b5e4519d76fd1
Vasileios Pasialiokis <whiterocket@outlook.com>
2017-08-09 03:52:46 +0300

    revamping

--- aooo2feed.js
+++ aooo2feed.js
@@ -1,89 +1,60 @@
 // jshint asi: true
 // jshint esversion: 6
-function main() {
+(function() {
 "use strict"
-
+// GLOBALS
 var DATA = {}
 
+const FEED_CSS = `body { background: #eee; }
+#loadline {
+	display: none;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 0.25em;
+	color: #aa0000
+}`
+
+const FEED_HTML = `<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>aooo2feed</title>
+	<style>${FEED_CSS}</style>
+</head>
+<body>
+	<div id="loadline"></div>
+	<section id="sidebar"></section>
+	<section id="mainfeed"></section>
+</body>
+</html>`
+
+// UTILITY FUNCTIONS
 function $(q) { return document.querySelector(q) }
 function $$(q) { return document.querySelectorAll(q) }
-
-function foreach (arr, fn) {
-	for (let i = 0; i < arr.length; i++) fn(arr[i], i, arr)
-}
-
-function some (arr, fn) {
-	for (let i = 0; i < arr.length; i++) {
-		if (fn(arr[i], i, arr)) return true
-	}
-	return false
-}
-
-function feed2arr (feed) {
-	return Object.keys(feed).map(key => {
-		return ({
-			"href": key,
-			"title": feed[key].title,
-			"id": feed[key].id })
-	})
-}
-
-function worksort (arr) {
-	return arr.sort((a,b) => { return a.id < b.d ? -1 : 1 })
-}
-
-function GET(URL, cb) {
-	const req = new XMLHttpRequest()
-	req.onload = function() { cb(req) }
-	req.open("GET", URL)
-	req.send()
-}
 
 function E(tagname, attrs, text, children) {
 	let elem = document.createElement(tagname)
 	if (attrs !== null) {
 		let keys = Object.keys(attrs)
-		keys.forEach(key => { elem[key] = attrs[key] })
-	}
+		keys.forEach(key => { elem[key] = attrs[key] }) }
 	if (text) elem.appendChild(document.createTextNode(text))
 	if (children) foreach(children, child => { elem.appendChild(child) })
-	return elem
-}
+	return elem }
 
-function parsework(work) {
-	const a = work.querySelector("h4.heading > a")
-	return { "href": a.href, "title": a.innerHTML }
-}
+function foreach (arr, fn) {
+	for (let i = 0; i < arr.length; i++) fn(arr[i], i, arr) }
 
-function add_tracked_page(what) {
-	if (!(what in DATA)) {
-		DATA[what] = {
-			"lastsync": Date.now(),
-			"sample": [],
-			"unread": {},
-		}
-	}
-}
+function some (arr, fn) {
+	for (let i = 0; i < arr.length; i++) if (fn(arr[i], i, arr)) return true
+	return false }
 
-function remove_tracked_page(what) { delete DATA[what] }
-
-function periodic_sync() {
-	const keys = Object.keys(DATA)
-	keys.forEach(key => {
-		if (Date.now() - DATA[key].lastsync < 1000*60*60*20) return
-		gimme_new(key, function (results, sample) {
-			Object.keys(results).forEach(item => {
-				DATA[key].unread[item] = results[item]
-			})
-			DATA[key].lastsync = Date.now()
-			DATA[key].sample = sample
-		})
-	})
-}
-
-function next_page(doc) {
-	return doc.querySelector("a[rel='next']").href
-}
+function GET(URL, cb) {
+	const req = new XMLHttpRequest()
+	req.onload = function() { cb(req) }
+	req.open("GET", URL)
+	req.send() }
 
 function gimme_new(key, cb) {
 	function do_stuff(req, isfirst) {
@@ -94,89 +65,56 @@ function gimme_new(key, cb) {
 				const pw = parsework(work)
 				sample.push(pw)
 				if (i >= 5) return true
-				else return false
-			})
+				else return false })
 		const flag = some(works, (work, i) => {
 			const pw = parsework(work)
 			if (existing.has(pw.href)) return true
 			else {
 				const d = Date.now()
 				results[pw.href] = {"title": pw.title, id: d+i}
-				return false
-			}
-		})
+				return false } })
 		if (flag || Object.keys(results).length >= 50) cb(results)
-		else GET(next_page(doc), req => do_stuff(req, false))
-	}
+		else GET(next_page(doc), req => do_stuff(req, false)) }
 
 	const parser = new DOMParser()
 	const existing = new Set(DATA[key].sample)
 	const results = {}
 	const sample = []
-	GET(key, req => do_stuff(req, true))
-}
+	GET(key, req => do_stuff(req, true)) }
 
-function save() {
-	window.localStorage.setItem("aooo2feed", JSON.stringify(DATA))
-}
+function parsework(work) {
+	const a = work.querySelector("h4.heading > a")
+	return { "href": a.href, "title": a.innerHTML } }
 
-function searchpath() { return window.location.pathname + window.location.search }
+function next_page(doc) { return doc.querySelector("a[rel='next']").href }
 
-function main() {
-	inject()
-}
+function searchpath() {
+	return window.location.pathname + window.location.search }
 
+function show(e) { e.style.display = "" }
+function hide(e) { e.style.display = "none" }
+
+// EVENT FUNCTIONS
 function add_search_clicked() {
 	if (searchpath() in DATA) {
 		remove_tracked_page(searchpath())
-		this.text = "[removed!]"
-	} else {
+		this.text = "[removed!]" }
+	else {
 		add_tracked_page(searchpath())
 		this.text = "[added!]"
-		periodic_sync()
-	}
-}
+		periodic_sync() } }
 
 function work_clicked() {
 	delete DATA[this.feed].unread[this.href]
-	this.remove()
-}
+	this.remove() }
 
-function inject() {
-	let inject_hash = { "/": inject_index, "/works/search": inject_search }
-	let fn = inject_hash[window.location.pathname]
-	if (fn) {
-		const item = window.localStorage.getItem("aooo2feed")
-		DATA = item ? JSON.parse(item) : {}
-		window.addEventListener("beforeunload", save)
-		periodic_sync()
-		fn()
-	}
-}
-
-function inject_index() {
-	let elem = $("#main > .splash")
-	elem.appendChild(aggregator_elem())
-}
-
-function inject_search() {
-	let elem = $("#main > h2.heading")
-	elem.appendChild(E("a", {
-		"href": "#",
-		"text": searchpath() in DATA ? "[remove search]" : "[track search]",
-		"style": "font-size: 10pt; margin-left: 1em;",
-		"onclick": add_search_clicked,
-	}))
-}
-
+// ELEMENT GENERATORS
 function aggregator_elem() {
 	const unread = []
 	Object.keys(DATA).forEach(feed => {
 		feed2arr(DATA[feed].unread).forEach(item => {
 			item.feed = feed
-			unread.push(item)
-		})
-	})
+			unread.push(item) }) })
 	worksort(unread)
 	const elems = [E("h3", null, "Tracked Searches")]
 	unread.forEach((work) => {
@@ -185,15 +123,33 @@ function aggregator_elem() {
 				"href": work.href,
 				"target": "_blank",
 				"onclick": work_clicked,
-				"feed": work.feed
-			},
-			work.title)]))
-	})
-	return E("div", {"className": "aggregator module odd"}, null, elems)
-}
+				"feed": work.feed },
+			work.title)])) })
+	return E("div", {"className": "aggregator module odd"}, null, elems) }
 
-main()
+// INJECTION FUNCTIONS
+function inject_feed() {
+	document.open()
+	document.write(FEED_HTML)
+	document.close() }
 
-}
+function default_inject() {}
 
-window.addEventListener("load", main)
+function inject_search() {
+	let elem = $("#main > h2.heading")
+	elem.appendChild(E("a", {
+		"href": "#",
+		"text": searchpath() in DATA ? "[remove search]" : "[track search]",
+		"style": "font-size: 10pt; margin-left: 1em;",
+		"onclick": add_search_clicked })) }
+
+function main() {
+	let inject_hash = {
+		"/aooo2feed": inject_feed,
+		"/works/search": inject_search }
+	let fn = inject_hash[window.location.pathname]
+	if (fn) fn()
+	else default_inject() }
+
+if (document.readysState === "complete") main()
+else window.addEventListener("load", main)})()
